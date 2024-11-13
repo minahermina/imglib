53aff2f8ee4346ac71610701d06c998ca006406d
minaalbertsaeed <mina.albert33@gmail.com>
2024-11-13 18:23:56 +0200

    Clean up main.c

--- main.c
+++ main.c
@@ -3,14 +3,9 @@
 
 #include "image.h"
 
-#define WIDTH  6 
-#define HEIGHT 6 
-
 int main (int argc, char const *argv[]) {
-    int x = 2, y = 0, channels = 2;
-    // ImagePtr img = create_img(WIDTH, HEIGHT, channels);
-    ImagePtr img = loadimg("./images/stop_2.ppm");
-    // print_img(img);
-
-
+    ImagePtr img = loadimg(argv[1]);
+    if (saveppm("./test.ppm", img)){
+        fprintf(stderr, "lol");
+    }
 }

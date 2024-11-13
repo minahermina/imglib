d0e34cda0515c979fdc1c056260c950106e7d048
minaalbertsaeed <mina.albert33@gmail.com>
2024-11-13 17:17:48 +0200

    Fix saveppm and loadppm issues
    They work now !!

--- image.c
+++ image.c
@@ -93,8 +93,6 @@ loadppm(const char* file)
         for(uint8_t channel = 0; channel < channels; channel++){
             pixel_values[channel] = buf[channel];
         }
-        uint16_t x = i % w;
-        uint16_t y = i / w;
         uint16_t x = i / w;
         uint16_t y = i % w;
         // printf("%hu %hu \n", x, y);
@@ -107,26 +105,33 @@ loadppm(const char* file)
     return img;
 }
 uint8_t saveppm(const char *file, ImagePtr img){
-    uint8_t px[3] = {};
+    uint16_t px[3] ;
+    unsigned char px_ch[3];
+    FILE *imgfile ;
+
     if(img == NULL){
         fprintf(stderr, "img is NULL!");
         return -1;
     }
 
-    FILE * imgfile = fopen(file, "wb");
+    imgfile = fopen(file, "wb");
     if(imgfile == NULL){
         fprintf(stderr, "failed to open file %s", file);
         return -1;
     }
+
     fprintf(imgfile, "P6\n%hu %hu\n255\n", img->width, img->height);
-    for (uint32_t i = 0; i < img->width * img->height; i+=3) {
-        for (uint8_t channel = 0; channel < img->channels; channel++) {
-            px[channel] =  img->data[i + channel];
+    for(size_t i = 0; i < img->height; i++) {
+        for(size_t j = 0; j < img->width; j++) { 
+            getpixel(img, i, j, px);
+            for(size_t k = 0; k < img->channels; k++) { 
+                px_ch[k] = (unsigned char)px[k];
+            }
+            fwrite(px_ch, sizeof(unsigned char), 3, imgfile);
         }
-        printf("pixel: %hhu %hhu %hhu\n", px[0], px[1], px[2]);
-        fwrite(&px, 1, 3, imgfile);
     }
     return 0;
+
 
 }
 

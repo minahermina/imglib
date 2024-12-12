4cae0ff2239fb036b9449fe0dd51a0dae79d36b8
minaalbertsaeed <mina.albert33@gmail.com>
2024-12-12 21:08:48 +0200

    Replace magic numbers defines with enum for more readable code

--- image.h
+++ image.h
@@ -2,16 +2,26 @@
 #define IMAGE_LIB
 #include <stdint.h>
 
-#define PPM_MAGIC_ASCII 0x50330A /* P3 */
-#define PPM_MAGIC_BIN   0x50360A /* P6 */
-#define PGM_MAGIC_ASCII 0x50320A /* P2 */
-#define PGM_MAGIC_BIN   0x50330A /* P5 */
+typedef enum {
+    IMG_UNKNOWN = -1,
+    IMG_PPM_BIN = 0x5036,    // P6
+    IMG_PPM_ASCII = 0x5033,  // P3
+    IMG_PGM_BIN = 0x5035,    // P5
+    IMG_PGM_ASCII = 0x5032,  // P2
+    IMG_PNG = 0x8950,        // \x89P
+    IMG_JPG = 0xFFD8,        // \xFF\xD8
+    IMG_BMP = 0x424D,        // BM
+    IMG_GIF = 0x4749,        // GI
+    IMG_TIFF = 0x4949        // II
+} ImgType;
 
 typedef struct {
-    uint16_t *data;
-    uint16_t  width;
-    uint16_t  height;
+    uint8_t *data;
+    uint32_t stride;
+    uint16_t width;
+    uint16_t height;
     uint8_t  channels;
+    ImgType type;
 } image;
 
 typedef image *ImagePtr;

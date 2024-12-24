c8fbb328d5b076f66048e5422719d1b847c97c13
minaalbertsaeed <mina.albert33@gmail.com>
2024-12-24 08:14:46 +0200

    Edit main.c

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <fcntl.h>
 
 #include "image.h"
 
 int main (int argc, char const *argv[]) {
-    ImagePtr img = loadimg(argv[1]);
-    if (saveppm("./test.ppm", img)){
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <image_file>\n", argv[0]);
+        return 1;
     }
+
+    ImagePtr img = loadimg(argv[1]);
+    if(img != NULL){
+        dispimg(img, "sxiv");
+    }
+    return 0;
 }

449e1adf533809591183f85d2bd51e43857fdf06
minaalbertsaeed <mina.albert33@gmail.com>
2025-02-14 12:21:10 +0200

    Clean up main.c

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 #include "image.h"
 
 int main(int argc, char const *argv[]) {
-    // Check if the correct number of arguments is provided
+    const char *out_file;
+    uint16_t x,y;
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <image_file>\n", argv[0]);
         return 1;
     }
 
-    // Load the image from the file specified in the command line argument
     ImagePtr img = loadimg(argv[1]);
     if (img == NULL) {
         fprintf(stderr, "Failed to load image: %s\n", argv[1]);
@@ -29,7 +30,6 @@ int main(int argc, char const *argv[]) {
     printf("Pixel at (0, 0): R=%d, G=%d, B=%d\n", pixel[0], pixel[1], pixel[2]);
 
     // Manipulate the image: Invert the colors of each pixel
-    uint16_t x,y;
     for (y = 0; y < img->height; y++) {
         for (x = 0; x < img->width; x++) {
 
@@ -52,13 +52,13 @@ int main(int argc, char const *argv[]) {
         }
     }
 
-    const char *output_file = "output_inverted.ppm";
-    if (saveimg(img, output_file) < 0) {
-        fprintf(stderr, "Failed to save modified image to %s.\n", output_file);
+    out_file = "output_inverted.ppm";
+    if (saveimg(img, out_file) < 0) {
+        fprintf(stderr, "Failed to save modified image to %s.\n", out_file);
         freeimg(img);
         return 1;
     }
-    printf("Modified image saved to %s.\n", output_file);
+    printf("Modified image saved to %s.\n", out_file);
 
     // Display the modified image using an external viewer
     printf("Displaying modified image...\n");
